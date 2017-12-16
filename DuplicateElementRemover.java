fb695113cc4f0edf926b370d946c54f8cf5cfd3d
Abhimanyu <abhimanyu081@outlook.com>
2017-12-16 09:21:23 +0530

    On branch master

---
+++ DuplicateElementRemover.java
@@ -0,0 +1,16 @@
+//DuplicateElementRemover
+class DuplicateElementRemover {
+    public int removeDuplicates(int[] nums) {
+        if(nums.length==0||nums.length==1){
+            return nums.length;
+        }
+        int j=0;
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i+1]!=nums[i]){
+                nums[j++]=nums[i];
+            }
+        }
+        nums[j++]=nums[nums.length-1];
+        return j;
+    }
+}
