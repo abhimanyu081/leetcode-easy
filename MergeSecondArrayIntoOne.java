cb2067fa391c6af705f0d254004e50fbb8848463
abhimanyu <abhimanyu081@outlook.com>
2021-01-18 00:46:21 +0530

    Create MergeSecondArrayIntoOne.java

---
+++ MergeSecondArrayIntoOne.java
@@ -0,0 +1,32 @@
+//https://leetcode.com/problems/merge-sorted-array/submissions/
+
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        
+        int p1=m-1;
+        int p2 = n-1;
+        int end = m+n-1;
+        
+        while(end>=0){
+            
+            if(p1>=0 && p2>=0){
+                
+                if(nums1[p1]>nums2[p2]){
+                    nums1[end--]=nums1[p1--];
+                }else{
+                    nums1[end--]=nums2[p2--];
+                }
+                
+            }else if(p2>=0){
+                
+                 nums1[end--]=nums2[p2--];
+                
+            }else{
+                break;
+            }
+            
+        }
+        
+        
+    }
+}
